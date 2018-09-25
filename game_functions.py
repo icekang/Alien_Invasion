6e3cf4624601d33e1d9028a4e7f086931ab7d5c5
namkang321 <namkang_chutisilp@hotmail.com>
2018-09-26 01:28:12 +0700

    Show ship_left

--- game_functions.py
+++ game_functions.py
@@ -162,10 +162,13 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, stats, sb, screen, ship, aliens, bullets):
     if stats.ship_left > 0:
         # Decrement ships left
         stats.ship_left -= 1
+
+        # Update scoreboard
+        sb.prep_ships()
 
         # Empty the list of aliens and bullets.
         aliens.empty()
@@ -181,7 +184,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         stats.game_active = False
         pygame.mouse.set_visible(True)
         
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets):
     """
     Check if alien is at the edge, then update the position of all aliens in the fleet
     """
@@ -191,6 +194,6 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 
     #Look for alien-ship collisions.
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        ship_hit(ai_settings, stats, sb, screen, ship, aliens, bullets)
         
     check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 import pygame.font
+from pygame.sprite import Group
+from ship import Ship
 
 class Scoreboard():
     def __init__(self, ai_settings, screen, stats):
@@ -15,7 +17,17 @@ class Scoreboard():
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
-
+        self.prep_ships()
+        
+    def prep_ships(self):
+        '''Show how many ships are left.'''
+        self.ships = Group()
+        for ship_number in range(self.stats.ship_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+            
     def prep_level(self):
         self.level_image = self.font.render(str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
 
@@ -23,6 +35,7 @@ class Scoreboard():
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+        
     def prep_high_score(self):
         """ Turn high score into rendered image"""
         high_score = int(round(self.stats.high_score, -1))
@@ -51,3 +64,4 @@ class Scoreboard():
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
